700f252bf08ebf04a34c3c40514f0573c5a0fad1
jimmylin192 <linxin@gmail.com>
2016-05-07 23:53:01 -0500

    preprocessing code

--- codebases/preprocess.py
+++ codebases/preprocess.py
@@ -1,6 +1,8 @@
 import sys
 import os
 from optparse import OptionParser
+import xml.etree.ElementTree as ET
+from HTMLParser import HTMLParser
 from sklearn.feature_extraction import DictVectorizer
 
 def parse_labels(string):
@@ -24,11 +26,12 @@ def strip(string):
         string = string.replace(x, " ")
     return string
 
+'''
 def read_xml(xml_files):
     # (gender, age, occupation, blogs)
     freqCounts = {}
     for fxml in xml_files:
-        labels = parse_labels(fxml.split("/")[-1])
+        #labels = parse_labels(fxml.split("/")[-1])
         fxml_handle = open(fxml, "r")
         for line in fxml_handle:
             if len(line) <= 1: continue
@@ -44,6 +47,37 @@ def read_xml(xml_files):
         #print "for XML file:", fxml
     for word in freqCounts:
         print word + ":" + str( freqCounts[word])
+'''
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def read_xml(xml_files):
+    # (gender, age)
+    id2tweets = {}
+    for fxml in xml_files:
+        fxml_base = os.path.basename(fxml)
+        user_id = fxml_base.strip(".xml")
+        print "Processing User:", user_id
+        tree = ET.parse(fxml)
+        documents = tree.getroot().find("documents")
+        all_tweets = []
+        for doc in documents.findall("document"):
+            tweet = doc.text
+            if tweet is None: continue
+            s = MLStripper()
+            s.feed(tweet)
+            tweet = s.get_data()
+            all_tweets.append(tweet)
+        id2tweets[user_id] = all_tweets
+
+    for user in id2tweets:
+        print user + ":", len(id2tweets[user])
 
 def read_truth(truth_file_path):
     labels_map = {}
@@ -58,23 +92,23 @@ def read_truth(truth_file_path):
     fh.close()
     return labels_map
 
-def main(options, in_filename, out_filename):
-    assert os.path.exists(in_filename), "xml_data not exists."
-    '''
-    if os.path.isdir(in_filename):
-        xml_files = [ in_filename+f for f in os.listdir(in_filename) \
-                 if os.path.isfile(in_filename+f) and f[-4:] == ".xml" ]
-    elif os.path.isfile(in_filename):
-        xml_files = [in_filename]
+def main(options, truth_file_path, xml_dir, out_filename):
+    assert os.path.exists(xml_dir), "xml_data not exists."
+    if os.path.isdir(xml_dir):
+        xml_files = [ os.path.join(xml_dir,f) for f in os.listdir(xml_dir) \
+                 if os.path.isfile(os.path.join(xml_dir,f)) and f[-4:] == ".xml" ]
+    elif os.path.isfile(xml_dir):
+        xml_files = [ xml_dir ]
     xml_data = read_xml (xml_files)
     '''
-    labels_map = read_truth(in_filename)
+    labels_map = read_truth(truth_file_path)
     for x in labels_map:
         print x, labels_map[x]
+    '''
 
 
 if __name__ == "__main__":
-    usage = "usage: python preprocess.py (options) [xml_data] [out_file]"
+    usage = "usage: python preprocess.py (options) [truth_file] [xml_dir] [out_file]"
     parser = OptionParser(usage=usage)
     parser.add_option("-b", "--bags-of-words", action="store_true", dest="BOG", \
                       default=False, help="option to turn on bags-of-words feature representation.")
@@ -83,9 +117,10 @@ if __name__ == "__main__":
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", help="verbose")
     parser.add_option("", "--mini", action="store_true", dest="mini", help="mini")
     (options, args) = parser.parse_args()
-    if len(args) != 2: 
+    if len(args) != 3: 
         parser.print_help()
         parser.error("Incorrect number of arguments.")
-    in_filename = args[0]
-    out_filename = args[1]
-    main(options, in_filename, out_filename)
+    truth_file_path = args[0]
+    xml_dir = args[1]
+    out_filename = args[2]
+    main(options, truth_file_path, xml_dir, out_filename)
